f2206218bfbacb9334e60e668b686db734daf6fc
Trung Hieu Nguyen Duong <mobile.hieunguyenduong@gmail.com>
2023-03-07 13:03:06 +0700

    structure file

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:dictionary/screens/home/home.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -17,19 +18,6 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
       ),
       home: const HomePage(),
-    );
-  }
-}
-
-class HomePage extends StatelessWidget {
-  const HomePage({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Simple Flutter app'),
-      ),
     );
   }
 }
