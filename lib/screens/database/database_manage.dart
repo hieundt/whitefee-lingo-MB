fde8acdc717df44422b8751e651220f5204c0f1e
hieundt <19521502@gm.uit.edu.vn>
2023-05-02 23:37:53 +0700

    adding data to db

---
+++ lib/screens/database/database_manage.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+class ManageVocabulary extends StatelessWidget {
+  const ManageVocabulary({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
+
+class ManageTest extends StatelessWidget {
+  const ManageTest({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
+
+class ManageUnit extends StatelessWidget {
+  const ManageUnit({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
